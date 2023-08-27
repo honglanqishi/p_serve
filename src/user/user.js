05cdfde0242fc9d041e0f386f11374f91d61c4bb
jiangsu <1071078@qq.com>
2023-08-27 19:16:53 +0800

    ttt

--- src/user/user.js
+++ src/user/user.js
@@ -75,6 +75,31 @@ router.post('/updateUserInfo', async (req, res) => {
 })
 
 
+//获取用户商店配置
+router.post('/getUserShopConf', async (req, res) => {
+    console.log('getUserShopConf', req.body)
+    let ret = await UserShopConf.findOne({
+        where: {
+            userID: req.body.userID
+        }
+    })
+    console.log(ret, '查询成功，打印getUserShopConf')
+    res.send(ret)
+})
+
+//更新用户商店配置
+router.post('/updateUserShopConf', async (req, res) => {
+    console.log('updateUserShopConf', req.body)
+    let ret = await UserShopConf.update(req.body, {
+        where: {
+            userID: req.body.userID
+        }
+    })
+    console.log(ret, '更新成功，打印updateUserShopConf')
+    res.send(ret)
+})
+
+
 // eslint-disable-next-line no-undef
 module.exports = {
     router
