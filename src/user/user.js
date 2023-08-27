05ca046285456e97a0842e783ac52a8178a5836b
jiangsu <1071078@qq.com>
2023-08-27 16:29:40 +0800

    1111

--- src/user/user.js
+++ src/user/user.js
@@ -1,11 +1,20 @@
 
-const { User } = require("../../db.js");
+const { User ,UserShopConf} = require("../../db.js");
 
 const express = require("express");
 
 let router = express.Router();
 
-
+//初始化用户商店配置
+var initUserShopConf = async (data) => {
+    let ret = await UserShopConf.create({
+        userID: data.data.userID,
+        dailyShop: "[1,1,1,1,1,1]",
+        lastRefreshTime: Date.now()
+    })
+    console.log(ret, 'initUserShopConf')
+    res.send(ret)
+}
 
 router.post('/getUserInfo', async (req, res) => {
     console.log('getUserInfo请求参数openid', req.body.openID)
@@ -26,8 +35,13 @@ router.post('/createUser', async (req, res) => {
     console.log('createUser', req.body)
     let ret = await User.create(req.body)
     console.log(ret, 'createUser')
+    if (ret) {
+        initUserShopConf(ret)
+    }
     res.send(ret)
 });
+
+
 
 module.exports = {
     router
