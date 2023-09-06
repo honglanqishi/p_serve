45f9ffe12be27f2d8dafa3d17e84f6a225d5a6e0
jiangsu <1071078@qq.com>
2023-09-06 16:53:36 +0800

    4212

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ app.get("/", async (req, res) => {
 // 小程序调用，获取微信 Open ID
 app.get("/api/wx_openid", async (req, res) => {
   if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
+    let data = {
+      oepnid: req.headers["x-wx-openid"]
+    }
+    res.send(data);
   }
 });
 
